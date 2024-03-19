c341498f44ef26bd6a3b51647323ab26a71db16c
Sujai-Rajan <rajan.suj@northeastern.edu>
2024-03-18 20:44:13 -0400

    Q1

--- HW_3/Q1.py
+++ HW_3/Q1.py
@@ -23,9 +23,26 @@ def q1_a(P: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     '''
 
     ### Enter code below
-    center = np.array([0,1,0])
-    normal = np.array([0,0,1])
+
+    # To see initial implementation, uncomment the following lines
+    # center = np.array([0,1,0])
+    # normal = np.array([0,0,1])
+
+
+    # Center the points around the mean
+    center = np.mean(P, axis=0)
+
+    # Find the covariance
+    covariance = np.cov(P.T)
+
+    # Apply SVD
+    U, S, Vt = np.linalg.svd(covariance)
+
+    # The normal to the plane is the last column of V
+    normal = Vt[2]
+        
     return normal, center
+
 
 
 def q1_c(P: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
@@ -46,6 +63,46 @@ def q1_c(P: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     '''
     
     ### Enter code below
-    center = np.array([0,1,0])
-    normal = np.array([0,0,1])
-    return normal, center
+    # center = np.array([0,1,0])
+    # normal = np.array([0,0,1])
+    # return normal, center
+
+
+    # Initialize variables
+    num_iterations = 1500
+    best_inliers = []
+    best_normal = None
+    best_center = None
+
+    for _ in range(num_iterations):
+        # Select 3 random points
+        random_indices = np.random.choice(P.shape[0], 3, replace=False)
+        sample_points = P[random_indices]
+
+        # Compute the centroid
+        centroid = np.mean(sample_points, axis=0)
+
+        # Center the points to the centroid
+        centered_points = sample_points - centroid
+
+        # Compute SVD
+        U, S, Vt = np.linalg.svd(centered_points)
+
+        # The normal vector is the last column of V
+        normal = Vt[-1]
+
+        # Calculate distances of all points to the plane
+        distances = np.abs((P - centroid).dot(normal))
+
+        # Determine inliers as points with distance below a threshold
+        inliers = np.where(distances < 0.01)[0]
+
+        # Update the best model if the current one has more inliers
+        if len(inliers) > len(best_inliers):
+            best_inliers = inliers
+            best_normal = normal
+            best_center = centroid
+
+    return best_normal, best_center
+
+    
